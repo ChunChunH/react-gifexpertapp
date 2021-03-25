768fd1a9f5c312117e03ccca8c1eda9ff9413e58
ChunChunH <agussolheredia@gmail.com>
2021-03-25 12:00:17 -0300

    cambios

---
+++ hooks-app/src/components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function CounterWithCustomHook() {
+    return (
+        <>
+            <h1>Counter with Custom Hook</h1>
+            <hr/>
+
+            <button className="btn btn-danger">+1</button>
+            <button className="btn btn-danger">-1</button>
+        </>
+    )
+}
+
+export default CounterWithCustomHook
